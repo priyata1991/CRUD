a050f4f8543ff04fe022b0ad27ed113483a54b5e
priyata1991 <67961221+priyata1991@users.noreply.github.com>
2020-07-08 11:07:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require('./models/db');
-
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyparser = require('body-parser');
+const Handlebars = require('handlebars')
 
 const employeeController = require('./controllers/employeeController');
 
@@ -13,7 +14,7 @@ app.use(bodyparser.urlencoded({
 }));
 app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/',handlebars: allowInsecurePrototypeAccess(Handlebars) }));
 app.set('view engine', 'hbs');
 
 app.listen(8080, () => {
